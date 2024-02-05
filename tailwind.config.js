11acad6ef488171ff51c8d585a6fa58b6bd51e06
Karl Lewis Doctolero <karllewistdoctolero@gmail.com>
2024-02-05 21:41:23 +0800

    delete button

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
 
-        "./resources/**/*.blade.php",
+      "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.vue",
       "./node_modules/flowbite/**/*.js"
