43167b18a337621a3d4ffe8ca70a057f6552480c
Karl Lewis Doctolero <karllewistdoctolero@gmail.com>
2024-03-18 23:05:45 +0800

    edit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,11 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="shortcut icon" href="{{asset('img/Logo.png')}}" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'FleetLink') }}</title>
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
 
 
         <!-- Fonts -->
